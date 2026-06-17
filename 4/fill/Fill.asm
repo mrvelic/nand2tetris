5005170b2967fc5ba38513ad4a6038243b3f18ec
Mirko Velic <mirko.velic@inlogik.com>
2026-06-17 16:38:16 +1000

    Project 4

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -1,11 +1,47 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/4/Fill.asm
 
 // Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(DISPRST)
+    // set sn to the beginning of the screen
+    @SCREEN
+    D=A
+    @sn
+    M=D
+
+(LOOP)
+    // if sn == KBD (end of display), reset sn loop counter
+    @sn
+    D=M
+    @KBD
+    D=D-A
+    @DISPRST
+    D;JEQ
+
+    // read the KBD value
+    @KBD
+    D=M
+    
+    // skip setting the -1 if the KBD is 0
+    @SETDISP
+    D;JEQ
+    D=-1
+
+(SETDISP)
+    // set the display value (0 if no key pressed, -1 if key pressed)
+    @sn
+    A=M
+    M=D
+
+    // sn = sn + 1
+    @sn
+    M=M+1
+    @LOOP
+    0;JMP
+
+
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -1,10 +1,38 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/4/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+    // clear memory
+    @n
+    M=0
+    @R2
+    M=0
+
+(LOOP)
+    // if n == R1 goto END
+    @n
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JEQ
+
+    // R2 = R2 + R0
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    // n = n + 1
+    @n
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
